bb26d48ee92cc5008b2615f174e36b8d8cbeb1a7
Palash Bansal <palashbansal96@gmail.com>
2023-12-23 00:15:25 +0530

    Version update

--- scripts/build-plugins.mjs
+++ scripts/build-plugins.mjs
@@ -1,5 +1,5 @@
 import {execEachPlugin} from "./utils.mjs";
 
 // Each plugin should have "prepare" that will also build the plugin
-execEachPlugin('npm ci') // install dependencies
+execEachPlugin('npm install') // install dependencies
 execEachPlugin('npm run docs')
