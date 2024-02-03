1ccb824e4fece4d22e72f5da0cebf89dcd98353a
Palash Bansal <palashbansal96@gmail.com>
2024-02-03 14:06:02 +0530

    Test ci changes

---
+++ scripts/each-plugin.mjs
@@ -0,0 +1,8 @@
+import {execEachPlugin} from "./utils.mjs";
+
+const command = process.argv.slice(2).join(' ')
+if(!command) throw new Error('Command is required')
+console.log(`Executing '${command}' in all plugins`)
+
+// Each plugin should have "prepare" that will also build the plugin
+execEachPlugin(`npm ${command}}`) // install dependencies
