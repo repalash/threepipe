5f927cc5b8978dfd8bcacba26dbc9df560fa0a91
Palash Bansal <palashbansal96@gmail.com>
2025-05-05 22:42:21 +0530

    Add build site script.

---
+++ scripts/build-site.sh
@@ -0,0 +1,9 @@
+set -euo pipefail
+
+npm run docs
+rm -rf _site
+mkdir _site
+cp -r src docs dist examples README.md LICENSE NOTICE _site
+cp -r website/.vitepress/dist/* _site
+mkdir -p _site/plugins
+find plugins -maxdepth 2 -type d \( -name dist -o -name docs -o -name src \) -exec sh -c "mkdir -p _site/{} && cp -r {} _site/{}/.." \;
