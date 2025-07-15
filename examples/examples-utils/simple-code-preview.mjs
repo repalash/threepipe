38cce6a7074b8f04221bcd1be162a58ea2081195
Palash Bansal <palashbansal96@gmail.com>
2025-07-15 23:03:24 +0530

    Minor fix for vite debugging

--- examples/examples-utils/simple-code-preview.mjs
+++ examples/examples-utils/simple-code-preview.mjs
@@ -6,12 +6,12 @@ const codePath = 'https://github.com/repalash/threepipe/tree/master/'
 
 const exampleScript = document.getElementById('example-script')
 const scripts = exampleScript && exampleScript.dataset.scripts ? exampleScript.dataset.scripts.split(';') : []
-if(exampleScript.textContent) scripts.push(exampleScript)
+if(exampleScript && exampleScript.textContent) scripts.push(exampleScript)
 const exampleStyle = document.querySelector('#example-style')
 const css = exampleStyle ? exampleStyle.textContent : ''
 const importMap = document.querySelector('script[type="importmap"]')
-const imports = importMap ? JSON.parse(importMap.textContent||'{}').imports||{} : {}
-Object.keys(imports).forEach((k)=>(k === 'threepipe' || k.startsWith('@threepipe/')) ? (imports[k] = 'https://unpkg.com/'+k+'/dist/index.mjs') : '') // required for codepen to work. this is done because plugins refer to threepipe as esm
+const imports = importMap ? JSON.parse(importMap.textContent || '{}').imports || {} : {}
+Object.keys(imports).forEach((k)=>(k === 'threepipe' || k.startsWith('@threepipe/')) ? (imports[k] = 'https://unpkg.com/' + k + '/dist/index.mjs') : '') // required for codepen to work. this is done because plugins refer to threepipe as esm
 Object.entries(imports).forEach(([k,v])=>imports[k] = v.replace(/^\.\/\.\.\/\.\.\//, rootPath)) // ./../../ -> rootPath
 
 // function replaceImports(code) {
@@ -20,11 +20,11 @@ Object.entries(imports).forEach(([k,v])=>imports[k] = v.replace(/^\.\/\.\.\/\.\.
 //         .replaceAll(` from '../../`, ` from '${rootPath}`)
 //         .replaceAll(` from '../`, ` from '${rootPath+examplePath}`)
 // }
-setupCodePreview(
+scripts.length && setupCodePreview(
     document.querySelector('.code-preview-container') || document.getElementById('canvas-container') || document.body,
     scripts,
     scripts.map(s=>s.textContent ? 'js' : s.split('.').pop()), // title
-    scripts.map(s=>(typeof s === 'string' && s.endsWith('.js')) ? s : (codePath+examplePath+window.location.pathname.split('/examples/').pop().replace('index.html', '')+(s.textContent ? 'index.html' : s))), // todo: github link
+    scripts.map(s=>(typeof s === 'string' && s.endsWith('.js')) ? s : (codePath + examplePath + window.location.pathname.split('/examples/').pop().replace('index.html', '') + (s.textContent ? 'index.html' : s))), // todo: github link
     (c) => '// Threepipe example: ' + window.location.href + '\n' + (c),
     {
         title: 'ThreePipe: ' + document.title,
@@ -35,5 +35,5 @@ setupCodePreview(
 <!-- Remove this when import maps will be widely supported -->
 <script async src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"></script>
 <script type="importmap">${JSON.stringify({imports}, null, 2)}</script>
-`
+`,
 );
