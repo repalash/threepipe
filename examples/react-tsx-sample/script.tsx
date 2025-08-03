cf162bf6d44c91368c7d6b7d65a805fe6a9b5482
Palash Bansal <palashbansal96@gmail.com>
2025-08-03 13:22:40 +0530

    Use newer React API in examples.

--- examples/react-tsx-sample/script.tsx
+++ examples/react-tsx-sample/script.tsx
@@ -1,13 +1,14 @@
 import {_testFinish, _testStart, LoadingScreenPlugin, ThreeViewer} from 'threepipe'
-// @ts-expect-error no need react here
 import React from 'react'
-// @ts-expect-error no need react-dom here
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
 
 function ThreeViewerComponent({src, env}: {src: string, env: string}) {
-    const canvasRef = React.useRef(null)
+    const canvasRef = React.useRef<HTMLCanvasElement>(null)
     React.useEffect(() => {
-        const viewer = new ThreeViewer({canvas: canvasRef.current,
+        if (!canvasRef.current) return
+
+        const viewer = new ThreeViewer({
+            canvas: canvasRef.current,
             plugins: [LoadingScreenPlugin],
         })
 
@@ -22,22 +23,21 @@ function ThreeViewerComponent({src, env}: {src: string, env: string}) {
             console.log('Loaded', model, env, viewer)
         })
         return () => {
+            // dispose the viewer and all its resources when component unmounts. You can also save the viewer instance somewhere and reuse it later if required instead of disposing.
             viewer.dispose()
         }
     }, [])
     return (
-        // @ts-expect-error react ts error
         <canvas id="three-canvas" style={{width: 800, height: 600}} ref={canvasRef} />
     )
 }
 
 async function init() {
-    ReactDOM.render(
+    createRoot(document.getElementById('root')!).render(
         <ThreeViewerComponent
             src={'https://threejs.org/examples/models/gltf/DamagedHelmet/glTF/DamagedHelmet.gltf'}
             env={'https://threejs.org/examples/textures/equirectangular/venice_sunset_1k.hdr'}
         />,
-        document.getElementById('root')
     )
 }
 
