8b5f34d902416ccfcd0b60c4786c2732d11fb26e
Palash Bansal <palashbansal96@gmail.com>
2025-08-06 21:25:57 +0530

    Update material configurator plugin

--- examples/material-configurator-plugin/script.ts
+++ examples/material-configurator-plugin/script.ts
@@ -1,5 +1,5 @@
 import {
-    _testFinish, _testStart,
+    _testFinish, _testStart, ContactShadowGroundPlugin,
     FrameFadePlugin,
     IObject3D,
     LoadingScreenPlugin,
@@ -9,13 +9,15 @@ import {
 } from 'threepipe'
 import {TweakpaneUiPlugin} from '@threepipe/plugin-tweakpane'
 import {MaterialConfiguratorPlugin} from '@threepipe/plugin-configurator'
+import {WatchHandsPlugin, SSReflectionPlugin, TemporalAAPlugin} from '@threepipe/webgi-plugins'
 
 async function init() {
 
     const viewer = new ThreeViewer({
         canvas: document.getElementById('mcanvas') as HTMLCanvasElement,
-        msaa: true,
-        plugins: [LoadingScreenPlugin, PickingPlugin, FrameFadePlugin, SSAAPlugin],
+        msaa: false,
+        renderScale: 'auto',
+        plugins: [LoadingScreenPlugin, PickingPlugin, FrameFadePlugin, SSAAPlugin, TemporalAAPlugin, WatchHandsPlugin, SSReflectionPlugin, ContactShadowGroundPlugin],
         dropzone: {
             addOptions: {
                 disposeSceneObjects: true,
@@ -26,6 +28,11 @@ async function init() {
     const ui = viewer.addPluginSync(new TweakpaneUiPlugin(true))
     const materialConfigurator = viewer.addPluginSync(new MaterialConfiguratorPlugin())
     materialConfigurator.enableEditContextMenus = true
+    materialConfigurator.animateApply = true
+    materialConfigurator.animateApplyDuration = 1000
+    ui.setupPluginUi(MaterialConfiguratorPlugin)
+    ui.setupPluginUi(PickingPlugin)
+    ui.appendChild(viewer.uiConfig)
 
     // await viewer.setEnvironmentMap('https://threejs.org/examples/textures/equirectangular/venice_sunset_1k.hdr')
 
@@ -33,16 +40,23 @@ async function init() {
     // You can use the editor to setup the materials in the UI and then load the model here.
     // Another way to load the material variations is to export a json file of the plugin from the editor and load it in the same way after loading the model.
     await viewer.load<IObject3D>(
-        'https://demo-assets.pixotronics.com/pixo/gltf/material_configurator.glb',
+        // 'https://demo-assets.pixotronics.com/pixo/gltf/material_configurator.glb',
+        'https://asset-samples.threepipe.org/demos/classic-watch.glb',
         // 'https://demo-assets.pixotronics.com/pixo/gltf/classic-watch.glb',
         {
             autoCenter: true,
             autoScale: true,
         })
-    viewer.scene.mainCamera.controls!.enableDamping = true // since its disabled in the file for some reason
-    ui.setupPluginUi(MaterialConfiguratorPlugin)
-    ui.setupPluginUi(PickingPlugin)
 
+    viewer.scene.mainCamera.controls!.enableDamping = true // since its disabled in the file for some reason
+
+    // to get the watch to show the correct hands, we need to set the offsets
+    const wh = viewer.getPlugin(WatchHandsPlugin)!
+    wh.invertAxis = true
+    wh.hourOffset = 10
+    wh.minuteOffset = 7
+    wh.secondOffset = 38
+    wh.analog = false
 }
 
 _testStart()
