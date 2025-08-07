42af4b30ec7009bb1ea4577b25291c05f34e1402
Palash Bansal <palashbansal96@gmail.com>
2025-08-07 19:30:56 +0530

    Add hierarchy UI plugin example.

---
+++ examples/hierarchy-ui-plugin/script.ts
@@ -0,0 +1,21 @@
+import {_testFinish, _testStart, LoadingScreenPlugin, PickingPlugin, ThreeViewer} from 'threepipe'
+import {TweakpaneUiPlugin} from '@threepipe/plugin-tweakpane'
+import {HierarchyUiPlugin} from '@threepipe/plugin-tweakpane-editor'
+
+async function init() {
+
+    const viewer = new ThreeViewer({
+        canvas: document.getElementById('mcanvas') as HTMLCanvasElement,
+        msaa: true,
+        plugins: [LoadingScreenPlugin, HierarchyUiPlugin, PickingPlugin],
+    })
+
+    const ui = viewer.addPluginSync(new TweakpaneUiPlugin(true))
+    ui.setupPluginUi(HierarchyUiPlugin, {expanded: true})
+    ui.setupPluginUi(PickingPlugin)
+
+    await viewer.load('https://asset-samples.threepipe.org/demos/classic-watch.glb')
+}
+
+_testStart()
+init().finally(_testFinish)
